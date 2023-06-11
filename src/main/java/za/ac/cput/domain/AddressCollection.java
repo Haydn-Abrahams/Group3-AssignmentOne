9829e3593030d2203e99fb0064d262b2f3f50fed
Cebisani Lamani <100792303+Cebisani0520@users.noreply.github.com>
2023-06-11 21:44:08 +0200

    New Domain Classes.

---
+++ src/main/java/za/ac/cput/domain/AddressCollection.java
@@ -0,0 +1,20 @@
+/** AddressCollection.java
+ * This is a POJO class
+ * This class is immutable (objects should not be changed)
+ * Cebisani Lamani (219104409)
+ * 10 June 2023
+ * */
+package za.ac.cput.domain;
+
+public class AddressCollection {
+    private String addressCollection_ID;
+    private String store_Name;
+    private String street_Address;
+    private String suburb;
+    private String city;
+    private String province;
+    private String country;
+
+
+
+}
